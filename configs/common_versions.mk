9d07b600b49d7292509d1a756f2db79a20f450a4
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-09-24 23:40:34 +0500

    Initial commit

---
+++ configs/common_versions.mk
@@ -0,0 +1,9 @@
+# Version information used on all builds
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03R BUILD_ID=JRO03R BUILD_VERSION_TAGS=release-keys BUILD_UTC_DATE=$(shell date +"%s")
+
+DATE = $(shell date +%Y%m%d)
+SUPERNEXUS_BUILD_VERSION = BUILD3
+
+# Rom Manager properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sn.version=SuperNexus-$(TARGET_PRODUCT)-$(SUPERNEXUS_BUILD_VERSION)-$(DATE)
