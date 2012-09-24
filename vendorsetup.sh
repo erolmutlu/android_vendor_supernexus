dd721b3b41e686aef9600baaf7588db9e6001de0
erolmutlu <mutlu@erolmutlu.com>
2012-09-25 00:34:15 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 add_lunch_combo I777-userdebug
-add_lunch_combo I9100-userdebug
+add_lunch_combo i9100-userdebug
 add_lunch_combo I9100G-userdebug
 add_lunch_combo I9300-userdebug
 add_lunch_combo N7000-userdebug
